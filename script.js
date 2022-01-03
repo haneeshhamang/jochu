bdb5eac9776916ae20634ed9cd36638c204f2041
Haneesh Bandaru <haneeshbandaru@gmail.com>
2022-01-03 22:47:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter UserName");
+  	if(a== "jochu")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
